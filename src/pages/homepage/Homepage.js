d8b1910ac65ddc201c98c7f8e329235dfe927c0d
sudhanshu mathur <sudhanshumathur01>
2020-12-11 12:24:00 +0530

    create react app and added the homepage component

---
+++ src/pages/homepage/Homepage.js
@@ -0,0 +1,43 @@
+import React from "react";
+import './Homepage.scss'
+function Homepage() {
+  return (
+
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className='title'>Buy hats</h1>
+            <span className ='subtitle'>Shop now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className='title'>Buy hats</h1>
+            <span className ='subtitle'>Shop now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className='title'>Buy hats</h1>
+            <span className ='subtitle'>Shop now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className='title'>Buy hats</h1>
+            <span className ='subtitle'>Shop now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className='title'>Buy hats</h1>
+            <span className ='subtitle'>Shop now</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Homepage;
